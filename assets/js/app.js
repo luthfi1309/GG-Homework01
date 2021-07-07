f15884948964eafa13b6a8ed359db2697bfff5e6
Muhammad Luthfi Dzulfikar <fdzule230@gmail.com>
2021-07-06 21:55:48 -0700

    Generasi Gigih-Homework01

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+console.log("app.js is active");
+// let btnSubmit = document.getElementById("btn-submit");
+// console.log(btnSubmit);
+
+let test = () => {
+    alert("holla")
+};
+//selection element
+let btnSubmit = document.getElementById("btn-submit");
+
+// do something if the button click
+btnSubmit.addEventListener("click", () => { 
+    alert("holla")
+});
